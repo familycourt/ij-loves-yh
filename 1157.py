104daa823d24802e59b497216e62d501be4b7467
ssongjay <jay5326@naver.com>
2022-05-29 03:37:02 +0900

    Create 1157.py

---
+++ 1157.py
@@ -0,0 +1,16 @@
+from itertools import count
+import sys
+import collections
+
+arr = input().lower()
+arr_list = list(set(arr))
+cnt = []
+
+for i in arr_list:
+    count = arr.count(i)
+    cnt.append(count)
+    
+if cnt.count(max(cnt)) >= 2:
+    print("?")
+else:
+    print(arr_list[(cnt.index(max(cnt)))].upper())
